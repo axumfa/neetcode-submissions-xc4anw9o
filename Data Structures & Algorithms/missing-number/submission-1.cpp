f4a231d06a5c13a26fbae1b5721b24a01a92ab20
Umid Akhmedov <umidakhmedov2603@gmail.com>
2026-04-18 01:59:13 +0500

    Add: missing-number - submission-1

---
+++ Data Structures & Algorithms/missing-number/submission-1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int xor_all = 0;
+        int n = nums.size();
+
+        for(int i = 0; i <= n; i++)
+            xor_all ^= i;
+        
+        for(int num : nums)
+            xor_all ^= num;
+        
+        return xor_all;
+    }
+
+};

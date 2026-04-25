cf278f6f9e319f6c8016197a4c31a23e23b6c1d9
Umid Akhmedov <umidakhmedov2603@gmail.com>
2026-04-25 23:21:21 +0500

    Add: palindromic-substrings - submission-1

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int countSubstrings(string s) {
+        int res = 0;
+        int size = s.size();
+
+        for(int i = 0; i < size; i++)
+        {
+            int l = i, r = i;
+            while(l >= 0 && r < size && s[l--] == s[r++])
+                res++;
+            
+            l = i, r = i + 1;
+            while(l >= 0 && r < size && s[l--] == s[r++])
+                res++;
+        }
+
+        return res;
+    
+    }
+
+};

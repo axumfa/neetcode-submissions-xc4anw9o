3dc74fa10549938a98d1c0acbedc57a38f992ae9
Umid Akhmedov <umidakhmedov2603@gmail.com>
2026-07-05 23:20:05 +0500

    Add: n-th-tribonacci-number - submission-0

---
+++ Data Structures & Algorithms/n-th-tribonacci-number/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        
+        if(n == 0) return 0;
+        if(n < 3) return 1;
+        
+        vector<int> memo(n + 1);
+        memo[0] = 0;
+        memo[1] = 1;
+        memo[2] = 1;
+
+        for(int i = 3; i <= n; i++)
+            memo[i] = memo[i - 1] + memo[i - 2] + memo[i - 3];
+        
+        return memo[n];
+        
+    }
+};

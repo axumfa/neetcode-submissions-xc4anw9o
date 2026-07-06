f274341ca270b7252732cf604f7ebb6489ce0a78
Umid Akhmedov <umidakhmedov2603@gmail.com>
2026-07-06 23:15:38 +0500

    Add: house-robber - submission-5

---
+++ Data Structures & Algorithms/house-robber/submission-5.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        
+        int size = nums.size();
+        
+        if(size == 1)
+            return nums[0];
+        nums[1] = max(nums[0], nums[1]);
+
+        for(int i = 2; i < size; i++)
+        {
+            nums[i] = max(nums[i - 1], nums[i] + nums[i - 2]);
+        }
+
+        return nums[size - 1];
+    }
+};

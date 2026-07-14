f75e8c0b029969ce3d79759793106a8efbd15b79
Umid Akhmedov <umidakhmedov2603@gmail.com>
2026-07-14 23:44:11 +0500

    Add: combinations - submission-1

---
+++ Data Structures & Algorithms/combinations/submission-1.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> combine(int n, int k) {
+        vector<int> path;
+        vector<vector<int>> res;
+
+        backtrack(1, n, k, path, res);
+        return res;
+    }
+
+    void backtrack(int start, int n, int k, vector<int>& path, vector<vector<int>>& res)
+    {
+        if(path.size() == k)
+        {
+            res.push_back(path);
+            return;
+        }
+
+        for(int i = start; i <= n; i++)
+        {
+            path.push_back(i);
+            backtrack(i + 1, n, k, path, res);
+            path.pop_back();
+        }
+
+    }
+};
